2a7c5d12f9f7c578817921d8eaea6717c138f42b
Hasibur Rahman Ridoy <122957060+Hasibur-ridoy@users.noreply.github.com>
2024-08-30 16:27:45 +0600

    Add files via upload

---
+++ Water container/Water container.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+
+        l,r = 0, len(height) - 1
+
+        while l < r:
+            area = (r-l) * min(height[r], height[l])
+            res = max(res, area)
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return res
