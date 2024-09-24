0b6ab7991ab465babb835fd0631034cab7ffb07e
Hasibur Rahman Ridoy <122957060+Hasibur-ridoy@users.noreply.github.com>
2024-09-24 18:28:14 +0600

    Add files via upload

---
+++ InvertBinaryTree/InverseBinaryTree.java
@@ -0,0 +1,12 @@
+
+public class InverseBinaryTree {
+    public TreeNode invertTree(TreeNode root){
+        if (root == null){
+            return null;
+        }
+        TreeNode node = new TreeNode(root.val);
+        node.right = invertTree(root.left);
+        node.left = invertTree(root.right);
+        return node;
+    }
+}
