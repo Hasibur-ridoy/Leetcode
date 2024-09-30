af7db91711f9942f66ccf6d4f19b381ec74dd1a2
Hasibur Rahman Ridoy <122957060+Hasibur-ridoy@users.noreply.github.com>
2024-09-30 16:26:26 +0600

    Add files via upload

---
+++ Maximum depth of binary tree/BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
